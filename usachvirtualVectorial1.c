74aa564368a30ec6f5fdc1aee292dda670390504
BastianToro <bastian.toro@usach.cl>
2014-12-08 17:04:08 -0300

    Update usachvirtualVectorial1.c
    -Se agrego getopt.
    - Se agrego lectura de archivo para el arreglo.

--- usachvirtualVectorial1.c
+++ usachvirtualVectorial1.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <malloc.h>
-#include <math.h>
 #include <xmmintrin.h>
 #include <emmintrin.h>
 #include <pmmintrin.h>
-
-#define N 9
-
+#include <math.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
 
 __m128 funcion(__m128 v1, __m128 v2);
 float largoMin(float a, float b);
@@ -14,65 +12,98 @@ float largoMin(float a, float b);
 
 int main(int argc, char *argv[])
 {
-    float a[N];
-    float b[N];
+	FILE *fichero;
+	int sgte_opcion;
+	char *nombre;
+	char *tipo = "f:";
+	while(1){
+		sgte_opcion = getopt_long(argc,argv,tipo,NULL,NULL);
+		if(sgte_opcion == -1)break;
+		switch(sgte_opcion){
+			case 'f': 	{nombre = malloc(strlen(optarg));
+					strcpy(nombre,optarg);	
+					fichero = fopen(nombre,"r");
+					int numero,N=0;
+    					if (fichero == NULL){
+						return 0;
+					}
+					else{
+						while(!feof(fichero)){
+						    fscanf(fichero,"%d",&numero);
+						    N = N + 1;
+						}
+					 }
+					 fclose(fichero);
+					 N = N-1;
+					float a[N];
+				    float b[N];
 
-    for(size_t i = 0; i < N; i++){
-        a[i] = i + 1;
-    }
+				    for(size_t i = 0; i < N; i++){
+					a[i] = i + 1;
+				    }
 
-    for(size_t i = 0; i < N; i++){
-        b[i] = i + 1;
-    }
+				    for(size_t i = 0; i < N; i++){
+					b[i] = i + 1;
+				    }
 
-    /*se toma como n en la sumatoria, el largo menor entre los 2
-    vectores, ya que si uno es mas largo que el otro habrian
-    datos que operarian con datos que no existen*/
-    float largo = (largoMin(sizeof(a),sizeof(b)))/4;
+				    /*se toma como n en la sumatoria, el largo menor entre los 2
+				    vectores, ya que si uno es mas largo que el otro habrian
+				    datos que operarian con datos que no existen*/
+				    float largo = (largoMin(sizeof(a),sizeof(b)))/4;
 
-    __m128 v1;
-    __m128 v2;
-    __m128 sumatoria1;
-    __m128 sumatoria2;
+				    __m128 v1;
+				    __m128 v2;
+				    __m128 sumatoria1;
+				    __m128 sumatoria2;
 
-    for(size_t j=0;j<4;j++){//se inicializan en 0 las variables que contendran
-        sumatoria1[j]=0.0;  //la suma temporal de las funciones
-        sumatoria2[j]=0.0;
-    }
-    for(size_t j=0;j<largo;j=j+4){
-        if((largo/4)>=1){
-            v1 = _mm_load_ps(&a[j]);//se cargan en v1 y v2 los arreglos a[] y b[]
-            v2 = _mm_load_ps(&b[j]);
-            /*se llama a funcion que hace lo matematico. 
-             en sumatoria2 se van calculando los cuadrados de cada termino*/
-            sumatoria1 = _mm_add_ps(sumatoria1,funcion(v1,v2));
-            sumatoria2 = _mm_add_ps(sumatoria2,_mm_mul_ps(v1,v1));
-        }
-        /*si el arreglo no es multiplo de 4, se calcularan de forma
-        escalar los terminos restantes que son 3 o menos*/
-        else{
-            for(size_t k=j;k<largo;k++){
-                sumatoria1[k] = sumatoria1[k] + a[k] / 2 + sqrt(b[k]);
-                sumatoria2[k] = sumatoria2[k] + a[k] * a[k];
-            }
-        }
-    }
-    /*se calculan la suma de los 4 valores del vector, que en total conforman
-    el total de la funcion*/
-    float acc = 0.0;
-    float acc2 = 0.0;
-    for(size_t l=0;l<4;l++){
-        acc = acc + sumatoria1[l];
-        acc2= acc2+ sumatoria2[l];
-    }
+				    for(size_t j=0;j<4;j++){//se inicializan en 0 las variables que contendran
+					sumatoria1[j]=0.0;  //la suma temporal de las funciones
+					sumatoria2[j]=0.0;
+				    }
+				    for(size_t j=0;j<largo;j=j+4){
+					if((largo/4)>=1){
+					    v1 = _mm_load_ps(&a[j]);//se cargan en v1 y v2 los arreglos a[] y b[]
+					    v2 = _mm_load_ps(&b[j]);
+					    /*se llama a funcion que hace lo matematico. 
+					     en sumatoria2 se van calculando los cuadrados de cada termino*/
+					    sumatoria1 = _mm_add_ps(sumatoria1,funcion(v1,v2));
+					    sumatoria2 = _mm_add_ps(sumatoria2,_mm_mul_ps(v1,v1));
+					}
+					/*si el arreglo no es multiplo de 4, se calcularan de forma
+					escalar los terminos restantes que son 3 o menos*/
+					else{
+					    for(size_t k=j;k<largo;k++){
+						sumatoria1[k] = sumatoria1[k] + a[k] / 2 + sqrt(b[k]);
+						sumatoria2[k] = sumatoria2[k] + a[k] * a[k];
+					    }
+					}
+				    }
+				    /*se calculan la suma de los 4 valores del vector, que en total conforman
+				    el total de la funcion*/
+				    float acc = 0.0;
+				    float acc2 = 0.0;
+				    for(size_t l=0;l<4;l++){
+					acc = acc + sumatoria1[l];
+					acc2= acc2+ sumatoria2[l];
+				    }
 
-    /*anteriormente se descompuso el problema, se factorizo la resta acc, que
-    se restaba n veces en acc2, en donde n es el largo de acc2*/
-    float resultadoFinal = acc2 - largo*acc;
+				    /*anteriormente se descompuso el problema, se factorizo la resta acc, que
+				    se restaba n veces en acc2, en donde n es el largo de acc2*/
+				    float resultadoFinal = acc2 - largo*acc;
 
-    printf("Restulado Final = %f\n",resultadoFinal);
-        
-    return 0;
+				    printf("Restulado Final = %f\n",resultadoFinal);
+
+
+					}
+				break;
+			case '?': printf("valor no valido\n");
+				break;
+		}
+
+	}
+	free(nombre);
+
+	return 0;
 }
 
 __m128 funcion(__m128 v1, __m128 v2){
